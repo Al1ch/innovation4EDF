bf89918bb02575e45c643b0dc900c19de35e6379
Alain Chea <alain.chea@efrei.net>
2023-09-11 11:41:15 +0200

    feat(init): add libraries

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+
+	webpack(config, _options) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: ['@svgr/webpack'],
+		},
+		);
+
+		return config;
+	},
+	experimental: {
+		serverActions: true,
+	  },
+}
 
 module.exports = nextConfig
