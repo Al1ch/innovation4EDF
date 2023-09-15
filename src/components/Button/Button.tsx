eec2e58613ad47de2a7dc60f8606698da56fa448
Alain Chea <alain.chea@efrei.net>
2023-09-15 15:02:20 +0200

    feat(components): make components more reusable

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,15 +1,31 @@
 "use client";
 import React, { ComponentPropsWithoutRef } from "react";
 import styles from "./Button.module.scss";
-import Link from "next/link";
+import { usePathname } from "next/navigation";
+import cn from "classnames";
 
 type Props = ComponentPropsWithoutRef<"button"> & {
   children: React.ReactNode;
+  size?: string;
+  backgroundColor?: string;
+  handleClick?: (id: number, pathName: string) => void;
 };
 
-const Button = ({ children, ...props }: Props) => {
+const Button = ({
+  children,
+  onChange: handleClick,
+  size = "md",
+  backgroundColor = "white",
+  ...props
+}: Props) => {
+  const pathName = usePathname();
+
   return (
-    <button className={styles.container} {...props}>
+    <button
+      className={cn(styles.container, styles[size], styles[backgroundColor])}
+      {...props}
+      onClick={() => handleClick && handleClick(1, pathName)}
+    >
       <span className={styles.link}>{children}</span>
     </button>
   );
